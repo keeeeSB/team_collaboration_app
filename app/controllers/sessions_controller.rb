14229b10dafcd27f294d9cd23add00277c24e7cf
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-18 13:30:25 +0900

    ログイン、ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      flash[:success] = "ログインしました。"
+      redirect_to user
+    else
+      flash.now[:danger] = "ログインできませんでした。"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    flash[:success] = "ログアウトしました。"
+    reidrect_to root_path, status: :see_other
   end
 end
