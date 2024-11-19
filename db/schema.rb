ccff735e54f9e54670750f2686dcc064d84fb0a1
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-19 12:05:35 +0900

    Teamモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_18_030743) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_19_030457) do
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
