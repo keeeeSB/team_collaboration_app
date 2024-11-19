99a7701a34f2f21e2d0be87914d898228ded0b03
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-19 12:30:58 +0900

    チーム作成ページの作成

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,28 @@
 class TeamsController < ApplicationController
+  before_action :require_login
+
   def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      flash[:success] = "チームを作成しました。"
+      redirect_to @team
+    else
+      flash.now[:danger] = "チームを作成できませんでした。"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @team = Team.find(params[:id])
   end
+
+  private
+
+    def team_params
+      params.require(:team).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'teams/new'
-  get 'teams/show'
   root   "users#new"
   get    "/signup", to: "users#new"
   post  "/signup",  to: "users#create"
@@ -8,4 +6,5 @@ Rails.application.routes.draw do
   post   "/login",  to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users
+  resources :teams
 end
